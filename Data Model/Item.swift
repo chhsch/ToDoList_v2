682f9f8dfc63f3e1172421cd475b74fc4784f83a
Hathaway <hathaway1214@gmail.com>
2024-04-01 02:54:36 -0400

    Initial commit

---
+++ Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Hathaway on 2024/3/29.
+//  Copyright © 2024 App Brewery. All rights reserved.
+//
+
+import Foundation
+class Item: Encodable{
+    var title: String = ""
+    var done: Bool = false
+}
+
